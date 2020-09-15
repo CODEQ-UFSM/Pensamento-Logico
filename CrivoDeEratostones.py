2d9c5f1a2b277fff61e7b155a4aa98875e3ef4bd
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2020-09-15 15:27:19 -0300

    Create CrivoDeEratostones.py

---
+++ CrivoDeEratostones.py
@@ -0,0 +1,11 @@
+final = 120
+limite = int((final)**0.5)
+
+numeros = list(range(2, final + 1))
+
+for i in range(2, limite + 1):
+    for j in numeros:
+        if j % i == 0 and j != i:
+            numeros.remove(j)
+
+print(numeros)
