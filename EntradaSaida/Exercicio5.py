b3bfc1ad58af91fd779a296726851fe176f469d9
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2020-10-09 00:34:08 -0300

    amongus

--- EntradaSaida/Exercicio5.py
+++ EntradaSaida/Exercicio5.py
@@ -1,31 +1,21 @@
-# Idade em Anos, Meses e Dias
+# Probabilidade do Impostor
 #
-# Dificuldade: ☆☆☆☆☆
+# Dificuldade: ☆☆
 #
 # Enunciado:
-# Receba a idade do usuário em dias e imprima quantos anos, meses e dias o usuário tem. Desconsidere anos bissextos e
-# considere que todos os meses posuem 30 dias.
+# Você está jogando Among Us e quer saber qual a probabilidade de certo jogador ser um impostor. Faça um
+# programa que receba a quantidade de impostores e a quantidade de jogadores e imprima a probabilidade.
 #
-# +--------------------+------------------+
-# | Exemplo de entrada | Exemplo de saída |
-# +--------------------+------------------+
-# | 1500               | Qtd. anos: 4     |
-# |                    | Qtd. meses: 1    |
-# |                    | Qtd. dias: 10    |
-# +--------------------+------------------+
-# | 7400               | Qtd. anos: 20    |
-# |                    | Qtd. meses: 3    |
-# |                    | Qtd. dias: 10    |
-# +--------------------+------------------+
-# | 20000              | Qtd. anos: 54    |
-# |                    | Qtd. meses: 9    |
-# |                    | Qtd. dias: 20    |
-# +--------------------+------------------+
+# +--------------------+-----------------------------+
+# | Exemplo de entrada |       Exemplo de saída      |
+# +--------------------+-----------------------------+
+# | 2                  | A probabilidade é de 40.0 % |
+# | 5                  |                             |
+# +--------------------+-----------------------------+
+# | 3                  | A probabilidade é de 30.0 % |
+# | 10                 |                             |
+# +--------------------+-----------------------------+
+# | 3                  | A probabilidade é de 50.0 % |
+# | 6                  |                             |
+# +--------------------+-----------------------------+
 #
-# DICA: Para ler valores tipo float, use int(input()).
-# DICA: Procure na internet sobre a operação módulo/module que possui o símbolo % e retorna o resto da divisão entre
-#       dois números. Ex.: 10%3 -> 1.
-#
-
-# Escreva o programa aqui
-# idade = ...
