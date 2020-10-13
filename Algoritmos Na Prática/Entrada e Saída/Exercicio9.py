b97d80bd0e54a112f185848722609df3865ebf8f
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2020-10-13 12:35:09 -0300

    Update Exercicio9.py

---
+++ Algoritmos Na Prática/Entrada e Saída/Exercicio9.py
@@ -0,0 +1,30 @@
+# Aproximação de Stirling
+#
+# Dificuldade: ☆☆☆☆
+#
+# Enunciado:
+# Em matemática, a aproximação de Stirling é uma aproximação para fatoriais. É uma boa aproximação, levando a
+# resultados precisos (não exatos) mesmo para pequenos valores de n.
+#
+#        _______   /  n  \ n
+# n! ~ \/ 2 𝜋 n   |  ---  |
+#                  \  e  /
+#
+# Considerando e = 2.7182818 e 𝜋 = 3.1415926, faça um programa que receba o valor de n e imprima seu fatorial
+# aproximado pela fórmula de Stirling.
+#
+# +--------------------+---------------------------+
+# | Exemplo de entrada |      Exemplo de saída     |
+# +--------------------+---------------------------+
+# | 5                  | 5 ! ~ 118.01917312900217  |
+# +--------------------+---------------------------+
+# | 10                 | 10 ! ~ 3598695.9648128385 |
+# +--------------------+---------------------------+
+# | 4                  | 4 ! ~ 23.506175916798625  |
+# +--------------------+---------------------------+
+#
+# DICA: Para ler valores tipo float, use int(input()).
+# DICA: Esse exercício é para aprender a utilizar parênteses nas equações
+#
+
+# Escreva o programa aqui
