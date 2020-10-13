606b1e00d5a7074316143e5ca80958836191ffc7
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2020-10-13 16:54:29 -0300

    Add Aulas

---
+++ Algoritmos Na Prática/Funcoes/Exercicio1.py
@@ -0,0 +1,22 @@
+# Nome
+#
+# Dificuldade: ☆☆☆☆☆
+#
+# Enunciado:
+# Lorem ipsum dolor sit amet
+#
+# +--------------------+----------------------------+
+# | Exemplo de entrada |      Exemplo de saída      |
+# +--------------------+----------------------------+
+# | 2                  | Área:  12.5663704          | ESSAS TABELAS PODEM SER GERADAS EM https://www.tablesgenerator.com/text_tables
+# |                    | Volume:  33.51032106666667 |
+# +--------------------+----------------------------+
+# | 5                  | Área:  78.539815           |
+# |                    | Volume:  523.5987666666666 |
+# +--------------------+----------------------------+
+#
+# DICA: Para ler valores tipo float, use float(input()).
+#
+
+# Escreva o programa aqui
+# r = ...
