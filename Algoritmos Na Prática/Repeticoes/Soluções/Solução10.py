4b170caf24cbb3107795b3127f5d1d06e67ab0d1
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2020-10-13 20:13:00 -0300

    Update

--- Algoritmos Na Prática/Repeticoes/Soluções/Solução10.py
+++ Algoritmos Na Prática/Repeticoes/Soluções/Solução10.py
@@ -2,7 +2,7 @@
 #
 # DICA: Para que um print não quebre a linha (lêe-se: "dar um enter"), é preciso escrever print('texto aqui', end=' ')
 #       onde o end indica como o print deve terminar.
-# DICA: Para intencionalmente quebrar uma linha, use o comando print('\n')
+# DICA: Para intencionalmente quebrar uma linha, use o comando print('\n').
 #
 # ATENÇÃO: Se você estiver se perguntando "Porque não nos ensinaram a quebrar linha na aula?" você precisa entender que
 #          é impossível passar todos os comandos de uma vez só. Além disso, aqui temos uma aplicação para estes
